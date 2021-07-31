e1706723cfb7ba92011d158b3fd9b3214f5c31c0
Muritala David <davmuri1414@gmail.com>
2021-07-31 13:12:08 -0700

    add task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,22 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    
+    uint public todoCount = 0;
+
+    constructor() public {
+        addTask('Welcome to blockchain task');
+    }
+
+    struct Task {
+        uint id;
+        string description;
+        bool done;
+    }
+
+    mapping (uint => Task) public tasks;
+
+    function addTask(string memory _description) public {
+        uint id = ++todoCount;
+        tasks[todoCount] = Task(taskC, _description, false);
+    }
 }
